22307369eeae0a65d4c1c034542105f72d6714f1
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-04 16:48:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,9 @@ sudo apt-get update
 # Install Python3 pip
 sudo apt-get install python3-pip
 
-# Install TA-Lib dependencies
-sudo apt-get install libfreetype6-dev libpng-dev libopenblas-dev liblapack-dev gfortran pkg-config
 
-# Download TA-Lib, unpack it, compile it, and install it
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-tar xvzf ta-lib-0.4.0-src.tar.gz
-cd ta-lib/
-./configure --prefix=/usr
-make
-sudo make install
 
 # Install necessary Python packages
-pip3 install alpaca-trade-api pandas pytz yfinance ta-lib
+pip3 install alpaca-trade-api 
+pip3 install yfinance
+pip3 install pytz
