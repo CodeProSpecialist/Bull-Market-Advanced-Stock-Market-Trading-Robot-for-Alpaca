d11170d9cce042583a76dc66a04b855954b2574a
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-04 16:50:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# Update package lists
-sudo apt-get update
+# Check if Python is installed
+if command -v python3 &>/dev/null; then
+    echo Python 3 is installed
+else
+    echo Python 3 is not installed
+    sudo apt-get update
+    sudo apt-get install python3-all
+    sudo apt-get install python3-pip
+fi
 
-# Install Python3 pip
-sudo apt-get install python3-pip
+# Check if pip is installed
+if command -v pip3 &>/dev/null; then
+    echo pip3 is installed
+else
+    echo pip3 is not installed
+    sudo apt install python3-pip
+fi
+
+# Now we can install our Python 3 packages
+pip3 install --upgrade pip
 
 # Install necessary Python packages
 pip3 install alpaca-trade-api 
