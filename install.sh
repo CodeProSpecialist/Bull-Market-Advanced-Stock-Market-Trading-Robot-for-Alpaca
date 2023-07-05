23e74e67cc40830c4fee922a9afc187dceeaeb73
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-05 13:41:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,34 @@ then
 fi
 
 # Install the necessary python packages
-sudo pip3 install alpaca-trade-api yfinance pandas ta
+sudo pip3 install alpaca-trade-api yfinance pandas
+
+# Check if build-essential is installed
+if ! command -v gcc &> /dev/null
+then
+    echo "gcc could not be found. Attempting to install build-essential..."
+    sudo apt update
+    sudo apt install -y build-essential
+fi
+
+# Install TA-Lib
+echo "Installing TA-Lib..."
+cd /tmp
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+
+# Check if TA-Lib installed correctly
+if ! ldconfig -p | grep libta_lib.so &> /dev/null
+then
+    echo "TA-Lib could not be found. The installation failed..."
+    exit 1
+fi
+
+# Install TA-Lib python wrapper
+sudo pip3 install TA-Lib
 
 echo "Setup completed successfully."
