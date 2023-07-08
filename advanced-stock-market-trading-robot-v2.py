9c13bfb42fe4a4974e3bb5263014e2d887834c32
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-08 11:08:19 -0500

    Update advanced-stock-market-trading-robot-v2.py

--- advanced-stock-market-trading-robot-v2.py
+++ advanced-stock-market-trading-robot-v2.py
@@ -1,196 +1,264 @@
-import os
-import time
-from datetime import datetime
-import pytz
 import alpaca_trade_api as tradeapi
 import yfinance as yf
-import talib
+import pandas as pd
+from talib import MACD, RSI, BBANDS
+from datetime import datetime, timedelta
+import backtrader as bt
+import os
+import logging
+import pytz
+import time
+import sys
 
+# Initialize the Alpaca API
 APIKEYID = os.getenv('APCA_API_KEY_ID')
 APISECRETKEY = os.getenv('APCA_API_SECRET_KEY')
 APIBASEURL = os.getenv('APCA_API_BASE_URL')
+api = tradeapi.REST(APIKEYID, APISECRETKEY, APIBASEURL)
+
+current_time_zone = datetime.now(pytz.timezone('America/New_York'))
+
+current_time = datetime.now(pytz.timezone('America/New_York')).strftime("%A, %b-%d-%Y %H:%M:%S")
+
+# Set up logging
+logging.basicConfig(filename='stock_bot.log', level=logging.INFO, format='%(asctime)s %(message)s')
 
 
-class AlpacaBot:
-    def __init__(self, symbols):
-        self.api = tradeapi.REST(APIKEYID, APISECRETKEY, APIBASEURL)
-        self.symbols = symbols
+# Load stock symbols from a text file
+def load_symbols(filename):
+    symbols = []
+    try:
+        with open(filename, 'r') as file:
+            symbols = [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    return symbols
 
-    def run(self):
-        current_time = datetime.now(pytz.timezone('US/Eastern'))
-        print(f"Current Eastern Time: {current_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-        while True:
-            account = self.api.get_account()
-            positions = self.api.list_positions()
-            prices = self.get_current_prices()
+# Stock symbols to monitor
+SYMBOLS = load_symbols('successful-stocks-list.txt')
 
-            print("\nAccount Information:")
-            print(f"Current account cash: {account.cash}")
-            print(f"Day Trade Count: {account.daytrade_count} out of 3 maximum per 5 days. ")
 
-            print("\nPositions:")
-            for position in positions:
-                print(f"Symbol: {position.symbol}, Quantity: {position.qty}, Avg Entry Price: {float(position.avg_entry_price):.2f}")
+def get_current_time_zone():
+    current_time_zone = datetime.now(pytz.timezone('America/New_York'))
+    return current_time_zone
 
-            print("\nCurrent Prices:")
-            for symbol, price in prices.items():
-                print(f"Symbol: {symbol}, Price: {price:.2f}")
 
-            if self.is_market_open():
-                self.buy_signals()
-                self.sell_signals()
-            else:
-                print("Waiting for the stock market to open.")
+def get_current_time():
+    current_time = datetime.now(pytz.timezone('America/New_York')).strftime("%A, %b-%d-%Y %H:%M:%S")
+    return current_time
 
-            # Sleep for 1 minute before the next iteration
-            time.sleep(60)
 
-    def is_market_open(self):
-        clock = self.api.get_clock()
-        return clock.is_open
+def print_account_info():
+    # Get account details
+    account = api.get_account()
 
-    def bullish(self, symbol):
-        # Place your logic here for bullish signal
-        # We are using Moving Average Convergence Divergence (MACD), Relative Strength Index (RSI), and Bollinger Bands as indicators for buying signals
-        bars = yf.download(symbol, period="1mo")
-        close_prices = bars['Close']
-        macd, macdsignal, macdhist = talib.MACD(close_prices)
-        rsi = talib.RSI(close_prices)
-        upper, middle, lower = talib.BBANDS(close_prices)
+    # Print account information
+    print("\nAccount Information:")
+    print(f"{(current_time)}" )
+    print(f"Day Trade Count: {account.daytrade_count} out of 3 total Day Trades in 5 business days.")
+    print(f"Current Account Cash: ${float(account.cash):.2f}")
+    print("--------------------")
 
-        # Generate signals based on the indicators
-        macd_signal = macd[-1] > macdsignal[-1]  # MACD crosses above signal line
-        rsi_signal = rsi[-1] < 30  # RSI is below 30 (Oversold)
-        bollinger_signal = close_prices[-1] < lower[-1]  # Price crosses below lower Bollinger Band
+    # Log account information
+    logging.info("\nAccount Information:")
+    logging.info(f"{get_current_time()}")
+    logging.info(f"Day Trade Count: {account.daytrade_count} out of 3 total Day Trades in 5 business days.")
+    logging.info(f"Current Account Cash: ${float(account.cash):.2f}")
+    logging.info("--------------------")
 
-        return macd_signal and rsi_signal and bollinger_signal
 
-    def bearish(self, symbol):
-        # Place your logic here for bearish signal
-        # We are using Moving Average Convergence Divergence (MACD), Relative Strength Index (RSI), and Bollinger Bands as indicators for selling signals
-        bars = yf.download(symbol, period="1mo")
-        close_prices = bars['Close']
-        macd, macdsignal, macdhist = talib.MACD(close_prices)
-        rsi = talib.RSI(close_prices)
-        upper, middle, lower = talib.BBANDS(close_prices)
+def print_positions():
+    # Get current positions
+    positions = api.list_positions()
 
-        # Generate signals based on the indicators
-        macd_signal = macd[-1] < macdsignal[-1]  # MACD crosses below signal line
-        rsi_signal = rsi[-1] > 70  # RSI is above 70 (Overbought)
-        bollinger_signal = close_prices[-1] > upper[-1]  # Price crosses above upper Bollinger Band
+    # Print current positions
+    print("\nCurrent Positions:")
+    for position in positions:
+        symbol = position.symbol
+        current_price = float(position.current_price)
+        closing_price = float(position.prev_close_price)
+        print(f"Symbol: {symbol}, Current Price: ${current_price:.2f}, Yesterday's Closing Price: ${closing_price:.2f}")
+    print("--------------------")
 
-        return macd_signal and rsi_signal and bollinger_signal
+    # Log current positions
+    logging.info("\nCurrent Positions:")
+    for position in positions:
+        symbol = position.symbol
+        current_price = float(position.current_price)
+        closing_price = float(position.prev_close_price)
+        logging.info(
+            f"Symbol: {symbol}, Current Price: ${current_price:.2f}, Yesterday's Closing Price: ${closing_price:.2f}")
+    logging.info("--------------------")
 
-    def buy_signals(self):
-        for symbol in self.symbols:
-            # Get the current account info
-            account = self.api.get_account()
 
-            # Parse the string to get the cash as a float
-            cash = float(account.cash)
+class MyStrategy(bt.Strategy):
+    def __init__(self):
+        self.rsi = bt.indicators.RSI(self.data.close, period=14)
+        self.macd = bt.indicators.MACD(self.data.close)
+        self.bbands = bt.indicators.BollingerBands(self.data.close)
 
-            if self.bullish(symbol):
-                self.buy_stock(symbol, cash)
+    def next(self):
+        if not self.position:
+            # Buy condition
+            if self.data.close[0] > self.data.close[-1] * 1.09 and self.rsi < 30 and self.macd.macd > self.macd.signal and self.data.close < self.bbands.lines.bot:
+                self.buy()
+        else:
+            # Sell condition
+            if self.rsi > 70 and self.macd.macd < self.macd.signal and self.data.close > self.bbands.lines.top:
+                self.sell()
 
-    def sell_signals(self):
-        account = self.api.get_account()
-        positions = self.api.list_positions()
 
-        for position in positions:
-            if self.bearish(position.symbol):
-                self.sell_stock(position)
+def backtest(strategy, data):
+    cerebro = bt.Cerebro()
+    cerebro.addstrategy(strategy)
+    cerebro.adddata(data)
+    cerebro.run()
 
-    def buy_stock(self, symbol, cash):
-        # Get account and check day trade count
-        account = self.api.get_account()
-        if account.daytrade_count >= 3:
-            print("Day trade limit reached. Not buying.")
-            return
 
-        # Get the last closing price of the stock
-        bars = yf.download(symbol, period="1d")
-        price = bars['Close'].iloc[-1]
+def bullish(symbol):
+    # Place your logic here for bullish signal
+    # We are using Moving Average Convergence Divergence (MACD), Relative Strength Index (RSI), and Bollinger Bands as indicators for buying signals
+    bars = yf.download(symbol, period="1mo")
+    close_prices = bars['Close']
+    macd, macdsignal, macdhist = MACD(close_prices)
+    rsi = RSI(close_prices)
+    upper, middle, lower = BBANDS(close_prices)
 
-        # Calculate the maximum number of shares we can buy
-        num_shares = int(cash / price)
+    # Generate signals based on the indicators
+    macd_signal = macd[-1] > macdsignal[-1]  # MACD crosses above signal line
+    rsi_signal = rsi[-1] < 30  # RSI is below 30 (Oversold)
+    bollinger_signal = close_prices[-1] < lower[-1]  # Price crosses below lower Bollinger Band
 
-        # If we can't buy at least 1 share, then don't submit the order
-        if num_shares < 1:
-            print(f"Not enough cash to buy a single share of {symbol}")
-            return
+    return macd_signal and rsi_signal and bollinger_signal
 
-        # Submit the order
-        self.api.submit_order(
-            symbol=symbol,
-            qty=num_shares,
-            side='buy',
+
+def bearish(symbol):
+    # Place your logic here for bearish signal
+    # We are using Moving Average Convergence Divergence (MACD), Relative Strength Index (RSI), and Bollinger Bands as indicators for selling signals
+    bars = yf.download(symbol, period="1mo")
+    close_prices = bars['Close']
+    macd, macdsignal, macdhist = MACD(close_prices)
+    rsi = RSI(close_prices)
+    upper, middle, lower = BBANDS(close_prices)
+
+    # Generate signals based on the indicators
+    macd_signal = macd[-1] < macdsignal[-1]  # MACD crosses below signal line
+    rsi_signal = rsi[-1] > 70  # RSI is above 70 (Overbought)
+    bollinger_signal = close_prices[-1] > upper[-1]  # Price crosses above upper Bollinger Band
+
+    return macd_signal and rsi_signal and bollinger_signal
+
+
+def evaluate_stock(symbol):
+    df = yf.download(symbol, period="1y")
+    df["RSI"] = RSI(df["Close"], timeperiod=14)
+    df["MACD"], _, _ = MACD(df["Close"], fastperiod=12, slowperiod=26, signalperiod=9)
+    df["Upper Band"], df["Middle Band"], df["Lower Band"] = BBANDS(df["Close"], timeperiod=20)
+
+    data = bt.feeds.PandasData(dataname=df)
+
+    backtest(MyStrategy, data)
+
+    # Calculate the percentage change from the initial value to the final value
+    initial_value = df["Close"].iloc[0]
+    final_value = df["Close"].iloc[-1]
+    percent_change = (final_value - initial_value) / initial_value * 100
+
+    # Print evaluation results with bullish/bearish indication
+    print(f"Evaluation results for {symbol}:")
+    print(f"RSI: {df['RSI'].iloc[-1]:.2f}")
+    print(f"MACD: {df['MACD'].iloc[-1]:.2f}")
+    print(f"Bollinger Bands: {df['Upper Band'].iloc[-1]:.2f} - {df['Middle Band'].iloc[-1]:.2f} - {df['Lower Band'].iloc[-1]:.2f}")
+    print(f"Percentage change: {percent_change:.2f}%")
+    print(f"Bullish: {bullish(symbol)}")
+    print(f"Bearish: {bearish(symbol)}")
+    print("--------------------")
+
+    # Log evaluation results with bullish/bearish indication
+    logging.info(f"Evaluation results for {symbol}:")
+    logging.info(f"RSI: {df['RSI'].iloc[-1]:.2f}")
+    logging.info(f"MACD: {df['MACD'].iloc[-1]:.2f}")
+    logging.info(
+        f"Bollinger Bands: {df['Upper Band'].iloc[-1]:.2f} - {df['Middle Band'].iloc[-1]:.2f} - {df['Lower Band'].iloc[-1]:.2f}")
+    logging.info(f"Percentage change: {percent_change:.2f}%")
+    logging.info(f"Bullish: {bullish(symbol)}")
+    logging.info(f"Bearish: {bearish(symbol)}")
+    logging.info("--------------------")
+
+def buy_stock(symbol, cash):
+    # Get account and check day trade count
+    account = api.get_account()
+    if account.daytrade_count >= 2:
+        print("Day trade limit reached. Not buying.")
+        return
+
+    # Get the last closing price of the stock
+    bars = yf.download(symbol, period="1d")
+    price = bars['Close'].iloc[-1]
+
+    # Calculate the maximum number of shares we can buy
+    num_shares = int(cash / price)
+
+    # If we can't buy at least 1 share, then don't submit the order
+    if num_shares < 1:
+        print(f"Not enough cash to buy a single share of {symbol}")
+        return
+
+    # Submit the order
+    api.submit_order(
+        symbol=symbol,
+        qty=num_shares,
+        side='buy',
+        type='market',
+        time_in_force='day'
+    )
+    print(f"Submitted order to buy {num_shares} shares of {symbol}")
+
+
+def sell_stock(position):
+    # Get account and check day trade count
+    account = api.get_account()
+    if account.daytrade_count >= 3:
+        print("Day trade limit reached. Not selling.")
+        return
+
+    if position.qty > 0:
+        # Submit an order to sell all shares of this stock
+        api.submit_order(
+            symbol=position.symbol,
+            qty=position.qty,
+            side='sell',
             type='market',
             time_in_force='day'
         )
-        print(f"Submitted order to buy {num_shares} shares of {symbol}")
+        print(f"Submitted order to sell all shares of {position.symbol}")
 
-    def sell_stock(self, position):
-        # Get account and check day trade count
-        account = self.api.get_account()
-        if account.daytrade_count >= 3:
-            print("Day trade limit reached. Not selling.")
-            return
 
-        if position.qty > 0:
-            # Submit an order to sell all shares of this stock
-            self.api.submit_order(
-                symbol=position.symbol,
-                qty=position.qty,
-                side='sell',
-                type='market',
-                time_in_force='day'
-            )
-            print(f"Submitted order to sell all shares of {position.symbol}")
 
-    def get_current_prices(self):
-        prices = {}
+def monitor_stocks():
+    while True:
+        # Print account information
+        print_account_info()
 
-        for symbol in self.symbols:
-            ticker = yf.Ticker(symbol)
-            price = ticker.history(period="1d")['Close'].iloc[-1]
-            prices[symbol] = price
+        # Print current positions
+        print_positions()
 
-        return prices
+        # Evaluate and print monitored stocks
+        for symbol in SYMBOLS:
+            evaluate_stock(symbol)
+
+        # Sleep for 1 minute before checking again
+        time.sleep(60)
+
 
 if __name__ == "__main__":
-    symbols = []
     while True:
-        symbol = input("Enter a stock symbol (or 'exit' to finish): ")
-        if symbol == "exit":
-            break
-        symbols.append(symbol)
-
-    print("\nSymbols Entered:")
-    for i, symbol in enumerate(symbols, 1):
-        print(f"{i}. {symbol}")
-
-    while True:
-        print("\nMenu:")
-        print("1. Continue")
-        print("2. Edit Symbol")
-        choice = input("Select an option: ")
-
-        if choice == "1":
-            break
-        elif choice == "2":
-            print("\nEdit Symbol:")
-            for i, symbol in enumerate(symbols, 1):
-                print(f"{i}. {symbol}")
-
-            symbol_number = int(input("Enter the symbol number to edit: "))
-            if symbol_number in range(1, len(symbols) + 1):
-                new_symbol = input("Enter the new symbol: ")
-                symbols[symbol_number - 1] = new_symbol
-            else:
-                print("Invalid symbol number. Please try again.")
-        else:
-            print("Invalid option. Please try again.")
-
-    bot = AlpacaBot(symbols)
-    bot.run()
+        try:
+            monitor_stocks()
+        except Exception as e:
+            print(f"Error: {e}")
+            logging.error(f"Error: {e}")
+            # Sleep for 5 seconds before restarting the program
+            time.sleep(5)
+            continue
