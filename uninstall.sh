bbc2f94fdc2c2e2b7a4430c608cc886b1bcfdbfb
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-20 22:17:51 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,8 @@ echo "Uninstalling the extra software libraries that were installed."
 pip3 uninstall alpaca-trade-api 
 pip3 uninstall yfinance
 pip3 uninstall pytz
+pip3 uninstall pandas
+pip3 uninstall tradingview_ta
 
 sudo pip3 uninstall TA-Lib
 
