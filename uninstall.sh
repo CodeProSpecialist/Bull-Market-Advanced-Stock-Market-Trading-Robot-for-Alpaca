392f57bf6336c48967b185fe790d8d19a77410cb
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-29 20:17:01 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 echo "Uninstalling the extra software libraries that were installed."
 
-pip3 uninstall yfinance pandas pandas_ta alpaca-trade-api matplotlib pytz
+pip3 uninstall yfinance pandas pandas_ta alpaca-trade-api matplotlib pytz numpy
 
 echo "Uninstall completed successfully."
