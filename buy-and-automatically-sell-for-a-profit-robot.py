2f5c8a5c048423060c58e0b040db4c12e81454e6
codeprospecialist <droark28@hotmail.com>
2023-07-29 21:03:44 -0500

    Code bugs were fixed.

--- buy-and-automatically-sell-for-a-profit-robot.py
+++ buy-and-automatically-sell-for-a-profit-robot.py
@@ -207,6 +207,8 @@ def main():
                         make_order(api, symbol, qty, 'buy')
                         log_order(symbol, 'buy')
                         time.sleep(600)
+                        print(" Waiting for 10 minutes after placing the most recent buy stock order to allow the ")
+                        print(" account to update before placing more buy orders. ")
 
                 elif data['MACD_Sell_Signal_price'].iloc[-1] > 0:
                     qty = get_position_qty(api, symbol)
