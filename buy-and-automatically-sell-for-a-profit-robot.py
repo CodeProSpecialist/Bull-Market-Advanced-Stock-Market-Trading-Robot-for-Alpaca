0deeb0706dac586b7e9387756b4699b590d58f58
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-08-14 17:08:50 -0500

    Update buy-and-automatically-sell-for-a-profit-robot.py
    All brand new python 3 code. 
     So new, it is in beta testing.

--- buy-and-automatically-sell-for-a-profit-robot.py
+++ buy-and-automatically-sell-for-a-profit-robot.py
@@ -1,21 +1,12 @@
 import os
+import logging
+import pytz
 import time
-import pandas as pd
 import yfinance as yf
 import talib
+from datetime import datetime
 import alpaca_trade_api as tradeapi
-import pytz
-from datetime import datetime, timedelta, date
-from datetime import time as time2
-import matplotlib.pyplot as plt
-import numpy as np
-import logging
-
-plt.rcParams['figure.figsize'] = (20, 10)
-plt.style.use('fivethirtyeight')
-
-# Using pandas datareader override
-yf.pdr_override()
+from alpaca_trade_api import REST
 
 # Load environment variables for Alpaca API
 APIKEYID = os.getenv('APCA_API_KEY_ID')
@@ -26,14 +17,6 @@ APIBASEURL = os.getenv('APCA_API_BASE_URL')
 api = tradeapi.REST(APIKEYID, APISECRETKEY, APIBASEURL)
 
 eastern = pytz.timezone('US/Eastern')
-
-debug_mode = False
-
-global risk, symbol, data, df, stock_symbols, stock_data
-
-risk = 0.025
-
-filename1 = 'successful-stocks-list.txt'
 
 
 def stop_if_stock_market_is_closed():
@@ -68,246 +51,85 @@ def stop_if_stock_market_is_closed():
         time.sleep(60)  # Sleep for 1 minute and check again
 
 
-def load_stocks_list():
-    with open('successful-stocks-list.txt', 'r') as file:
-        return [line.strip() for line in file]
-
-SYMBOLS = []
-
-SYMBOLS = load_stocks_list()
+# Setting up logging
+logging.basicConfig(filename="log-file-of-buy-and-sell-signals.txt", level=logging.INFO, format="%(asctime)s - %(message)s")
 
 
-def get_stock_info(SYMBOLS):
-    end = date.today() + timedelta(days=1)
-    start = end - timedelta(days=364)
+def get_current_price(symbol):
+    data = yf.download(tickers=symbol, period="1d", interval="1m")
+    return float(data["Close"][-1])
 
-    stock_data = yf.download(SYMBOLS, start=start, end=end)  # This will include 'High' and 'Low' by default
-    return stock_data
+def buy_stock(symbol):
+    price = get_current_price(symbol)
+    cash = float(api.get_account().cash)
+    if cash > price:  # Checking if we have enough cash to buy 1 share
+        try:
+            api.submit_order(symbol=symbol, qty=1, side='buy', type='market', time_in_force='day')
+            logging.info(f"Bought {symbol} at {price}")
+            time.sleep(300)  # Wait for 5 minutes after buying
+        except Exception as e:
+            logging.error(f"Error buying {symbol}: {e}")
 
+def sell_stock(symbol):
+    positions = api.list_positions()
+    for position in positions:
+        if position.symbol == symbol:
+            try:
+                api.submit_order(symbol=symbol, qty=int(position.qty), side='sell', type='market', time_in_force='day')
+                logging.info(f"Sold {symbol}")
+            except Exception as e:
+                logging.error(f"Error selling {symbol}: {e}")
 
-def compute_macd(df):
-    for symbol in SYMBOLS:
-        close_data = df['Close'][symbol]
-        macd = talib.macd(close_data)
-        df['MACD', symbol] = macd['MACD_12_26_9']
-        df['MACDs', symbol] = macd['MACDs_12_26_9']
-        df['MACDh', symbol] = macd['MACDh_12_26_9']
-    return df
+with open("successful-stocks-list.txt", "r") as f:
+    symbols = f.read().splitlines()
 
-
-# the python code below will remove the stock from the text file after the buy order is placed
-def remove_symbol(symbol, filename1):
-    symbols = []
-    SYMBOLS = []
+while True:
     try:
-        with open(filename1, 'r') as file:
-            symbols = [line.strip() for line in file.readlines() if line.strip() != symbol]
-    except FileNotFoundError:
-        print(f"Error: File '{filename1}' not found.")
-        return
+        stop_if_stock_market_is_closed()
+        eastern = pytz.timezone('US/Eastern')
+        now = datetime.now(eastern)
+        current_time_str = now.strftime('%A, %B %d, %Y, %I:%M %p (Eastern Time)')
+        print(f"Current Time: {current_time_str}")
 
-    with open(filename1, 'w') as file:
-        for s in symbols:
-            file.write(s + "\n")
+        # Make sure to respect Alpaca's rate limits. The default rate limit for Alpaca's API is 200 requests per minute.
 
-    # Clear the 'symbols' and 'SYMBOLS' variables
-    SYMBOLS = load_stocks_list()
-
-    symbols.clear()
-    SYMBOLS.clear()
-
-    # Update the 'symbols' and 'SYMBOLS' variables with new information
-    symbols = load_stocks_list()
-    SYMBOLS = load_stocks_list()
-
-
-def MACD_Strategy(df, risk):
-    MACD_Buy = []
-    MACD_Sell = []
-    position = False
-
-    for i in range(0, len(df)):
-        if df['MACD_12_26_9'][i] > df['MACDs_12_26_9'][i]:
-            MACD_Sell.append(np.nan)
-            if position == False:
-                MACD_Buy.append(df['Close'][i])
-                position = True
-            else:
-                MACD_Buy.append(np.nan)
-        elif df['MACD_12_26_9'][i] < df['MACDs_12_26_9'][i]:
-            MACD_Buy.append(np.nan)
-            if position == True:
-                MACD_Sell.append(df['Close'][i])
-                position = False
-            else:
-                MACD_Sell.append(np.nan)
-        elif position == True and df['Close'][i] < MACD_Buy[-1] * (1 - risk):
-            MACD_Sell.append(df["Close"][i])
-            MACD_Buy.append(np.nan)
-            position = False
-        elif position == True and df['Close'][i] < df['Close'][i - 1] * (1 - risk):
-            MACD_Sell.append(df["Close"][i])
-            MACD_Buy.append(np.nan)
-            position = False
-        else:
-            MACD_Buy.append(np.nan)
-            MACD_Sell.append(np.nan)
-
-    df['MACD_Buy_Signal_price'] = MACD_Buy
-    df['MACD_Sell_Signal_price'] = MACD_Sell
-
-    return df
-
-
-# Your MACD strategy function
-def MACD_color(df):
-    MACD_color = []
-    for i in range(1, len(df)):  # Start from index 1 to avoid accessing index -1
-        if df['MACDh_12_26_9'][i] > df['MACDh_12_26_9'][i - 1]:
-            MACD_color.append(True)
-        else:
-            MACD_color.append(False)
-    return MACD_color
-
-
-def plot_graph(stocks):
-    atr_period = 22
-    atr_multiplier = 3
-
-    # If your data includes multiple stocks, iterate through the symbols
-    for symbol in stocks['Close'].columns:
-        data = pd.DataFrame({
-            'High': stocks['High'][symbol],
-            'Low': stocks['Low'][symbol],
-            'Close': stocks['Close'][symbol]
-        })
-
-        # Calculate ATR
-        data['ATR'] = talib.ATR(data['High'], data['Low'], data['Close'], timeperiod=atr_period)
-
-        # Calculate buy and sell signals
-        data['Buy_Signal'] = data['Close'] - atr_multiplier * data['ATR']
-        data['Sell_Signal'] = data['Close'] + atr_multiplier * data['ATR']
-
-        plt.figure(figsize=(10, 6))
-        plt.title(f"{symbol} Stock Price")
-        plt.plot(data['Close'], label="Close Price", alpha=0.5)
-        plt.plot(data['Buy_Signal'], label="Buy Signal", linestyle="--")
-        plt.plot(data['Sell_Signal'], label="Sell Signal", linestyle="--")
-        plt.xlabel('Date')
-        plt.ylabel('Close Price')
-        plt.legend(loc='best')
-        plt.show()
-
-
-# sell all owned stocks that lose value more than a strict minimum average of -1.0 percent: 0.99
-def sell_decreasing_stocks(api):
-    try:
+        # Checking and logging owned positions
         positions = api.list_positions()
         for position in positions:
             symbol = position.symbol
-            qty = int(position.qty)
-            current_price = api.get_latest_trade(symbol).price
-            purchase_price = float(position.avg_entry_price)
+            logging.info(f"Owned stock - Symbol: {symbol}, Avg purchase price: {position.avg_entry_price}, Current price: {get_current_price(symbol)}")
 
-            if current_price <= purchase_price * 0.99:  # 1.0% decrease
-                make_order(api, symbol, qty, 'sell')
-                log_order(symbol, 'Sold')
-                print(f"Sold {symbol} as the price decreased by 1.0% or more.")
+        for symbol in symbols:
+            data = yf.download(tickers=symbol, period="22d", interval="1d")
+            close = data['Close'].values
+            atr = talib.ATR(data['High'].values, data['Low'].values, close, timeperiod=22)
+            if close[-1] > close[-2] + 3 * atr[-2]:
+                logging.info(f"Buy Signal for {symbol} - Previous Close: {close[-2]}, Current Close: {close[-1]}, ATR: {atr[-2]}")
+                if api.get_account().daytrade_count < 3:  # Checking day trade count
+                    buy_stock(symbol)
+            elif close[-1] < close[-2] - 3 * atr[-2]:
+                logging.info(f"Sell Signal for {symbol} - Previous Close: {close[-2]}, Current Close: {close[-1]}, ATR: {atr[-2]}")
+                sell_stock(symbol)
+
+            # Print each symbol's name, previous buy signal price, previous buy signal date, and current price
+            print(f"Symbol: {symbol}, Previous Close: {close[-2]}, Current Price: {close[-1]}")
+
+        time.sleep(2)  # Wait for 2 seconds before the next iteration
     except Exception as e:
-        print(f"An error occurred while trying to sell decreasing stocks: {e}")
+        logging.error(f"Error in main loop: {e}")
+    except:
+        logging.error("Unexpected error in main loop.")
 
-
-def make_order(api, symbol, qty, side):
-    api.submit_order(
-        symbol=symbol,
-        qty=qty,
-        side=side,
-        type='market',
-        time_in_force='day'
-    )
-
-
-def log_order(symbol, action):
-    with open('log.txt', 'a') as f:
-        f.write(f"{datetime.now()}: {action} {symbol}\n")
-
-
-def check_cash(api, symbol):
-    account = api.get_account()
-    cash_available = float(account.cash)
-    last_price = api.get_last_trade(symbol).price
-    qty = int(cash_available / last_price)
-    return qty if qty > 0 else None
-
-
-def get_position_qty(api, symbol):
     try:
-        position = api.get_position(symbol)
-        return int(position.qty)
+        pass  # Placeholder for additional error handler
+
     except Exception as e:
-        return None
+        logging.error(f"Secondary Error: {e}")
 
-
-def backtest():
-    while True:
-        try:
-            stop_if_stock_market_is_closed()
-            global SYMBOLS  # Declare SYMBOLS as a global variable
-            SYMBOLS = load_stocks_list()
-
-            # Get data once and store it in all_data
-            all_data = get_stock_info(SYMBOLS)
-
-            print(f' Eastern Time: {datetime.now(eastern).strftime("%A, %B %d, %Y %I:%M:%S %p")}')
-
-            sell_decreasing_stocks(api)
-
-            positions = api.list_positions()
-
-            if positions:
-                print("Stocks in our Portfolio:")
-                for position in positions:
-                    print(f'{position.symbol}, Current Price: {round(float(position.current_price), 2)}')
-            else:
-                print("No stocks are in our portfolio..... not looking to sell stocks right now.")
-
-            # Check day trade count
-            account = api.get_account()
-            day_trade_count = account.daytrade_count
-            print(f"Day Trade Count: {day_trade_count} out of 3 in 5 business days. ")
-
-            if debug_mode:
-                plot_graph(all_data)
-
-            print("Stocks to buy:")
-            for symbol in SYMBOLS:
-                if day_trade_count <= 3:  # Check day trade count before buying
-                    try:
-                        df = all_data[all_data['symbol'] == symbol] # Assuming all_data contains data for all symbols and has a 'symbol' column
-
-                        # Apply both strategies
-                        df = MACD_Strategy(df, risk)
-
-                        for i in range(len(df)):
-                            print(f"Index: {i}, Date: {df.index[i]}, Buy Signal: {df['MACD_Buy_Signal_price'][i]}, Sell Signal: {df['MACD_Sell_Signal_price'][i]}")
-                            # Continue with the rest of the code as before
-
-                    except KeyError:
-                        print(f"An error occurred: {symbol}")
-
-            time.sleep(2)  # Sleep for 2 seconds and then check again
-
-        except Exception as e:
-            print(f'An error occurred: {e}')
-            time.sleep(2)
-
-
-if __name__ == "__main__":
     try:
-        backtest()
+        time.sleep(120)  # Sleep for 2 minutes
+    except Exception as e:
+        logging.error(f"Error during sleep: {e}")
 
-    except KeyboardInterrupt:
-        print('Interrupted by user')
-
-    time.sleep(2)
+    except:
+        logging.error("Unexpected error during sleep.")
