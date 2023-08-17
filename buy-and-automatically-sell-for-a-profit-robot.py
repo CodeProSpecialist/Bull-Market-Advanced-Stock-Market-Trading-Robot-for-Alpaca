de3135a5933a0db81a655d7407ab3b606f925699
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-08-17 15:48:03 -0500

    Update buy-and-automatically-sell-for-a-profit-robot.py

--- buy-and-automatically-sell-for-a-profit-robot.py
+++ buy-and-automatically-sell-for-a-profit-robot.py
@@ -90,7 +90,7 @@ def get_day_trades_count(api):
 def buy_stock(symbol, price, api):
     cash_available = float(api.get_account().cash)
     if cash_available > price and api.get_account().daytrade_count < 3:
-        qty = int(cash_available // price)
+        qty = int(cash_available / price)
         api.submit_order(
             symbol=symbol,
             qty=qty,
