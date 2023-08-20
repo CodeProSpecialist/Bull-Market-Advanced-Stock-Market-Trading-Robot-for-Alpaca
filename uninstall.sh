ad8395c9baed2e3f27b91ad585cd7f9211eeafc0
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-08-20 13:28:58 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 echo "Uninstalling the extra software libraries that were installed."
 
-pip3 uninstall yfinance alpaca-trade-api pytz  
+pip3 uninstall yfinance alpaca-trade-api pytz ta-lib 
 
 echo "Uninstall completed successfully."
