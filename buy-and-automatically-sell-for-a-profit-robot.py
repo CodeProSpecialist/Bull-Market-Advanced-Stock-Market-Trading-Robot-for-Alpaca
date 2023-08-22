32f56534bc1c931a58b83c34917cc92d1f9f29fe
codeprospecialist <droark28@hotmail.com>
2023-08-22 10:43:49 -0500

    Code bugs were fixed.

--- buy-and-automatically-sell-for-a-profit-robot.py
+++ buy-and-automatically-sell-for-a-profit-robot.py
@@ -174,26 +174,19 @@ def update_bought_stocks_from_api(conn):
     return bought_stocks
 
 
+# the below function was recommended by Artificial Intelligence
 def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock):
     with buy_sell_lock:
-        global cash_balance
-        global fractional_qty
-        global qty_of_one_stock
-        global today_date
-        global symbol
-        global current_price
-
         stocks_to_remove = []
 
         for symbol in stocks_to_buy:
             today_date = datetime.today().date()
             current_price = get_current_price(symbol)
             atr_low_price = get_atr_low_price(symbol)
-            cash_balance = round(float(api.get_account().cash), 2)
-            cash_available = cash_balance - bought_stocks.get(symbol, 0)[0] if symbol in bought_stocks else cash_balance
-            fractional_qty = (cash_available / current_price) * 0.025
+            cash_available = round(float(api.get_account().cash), 2)
+            cash_available -= bought_stocks.get(symbol, 0)[0] if symbol in bought_stocks else 0
             qty_of_one_stock = 1
-            # if cash_available > current_price:   # debug code is on this line to make sure stock is buying correctly
+
             if cash_available > current_price and current_price <= atr_low_price:
                 api.submit_order(symbol=symbol, qty=qty_of_one_stock, side='buy', type='market', time_in_force='day')
                 bought_stocks[symbol] = (round(current_price, 4), datetime.today().date())
@@ -206,9 +199,8 @@ def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock):
         print(f" {today_date} , Bought {qty_of_one_stock} shares of {symbol} at {current_price}")
         logging.info(f" {today_date} , Bought {qty_of_one_stock} shares of {symbol} at {current_price}")
         print(" Waiting 4 minutes after buying stock to allow the remote server to update the order in the account. ")
-        # the buy thread will stop and allow the sell_stocks thread to keep running
         time.sleep(240)  # sleep 240 seconds after each buy order
-        refresh_after_buy()  # this was recommended by Artificial Intelligence
+        refresh_after_buy()
 
 
 # the below variable and list refresh function was recommended by Artificial Intelligence
@@ -218,13 +210,9 @@ def refresh_after_buy():
     bought_stocks = update_bought_stocks_from_api()
 
 
+# the below function was recommended by Artificial Intelligence
 def sell_stocks(bought_stocks, buy_sell_lock):
     with buy_sell_lock:
-        global qty
-        global today_date
-        global symbol
-        global current_price
-
         for symbol, (bought_price, bought_date) in bought_stocks.items():
             current_price = get_current_price(symbol)
             atr_high_price = get_atr_high_price(symbol)
@@ -238,7 +226,7 @@ def sell_stocks(bought_stocks, buy_sell_lock):
                 print(
                     " Waiting 2 minutes after selling stock to allow the remote server to update the order in the account. ")
                 time.sleep(120)  # sleep 120 seconds after each sell order
-                refresh_after_sell()  # this was recommended by Artificial Intelligence
+                refresh_after_sell()
 
 
 # the below variable and list refresh function was recommended by Artificial Intelligence
@@ -247,20 +235,11 @@ def refresh_after_sell():
     bought_stocks = update_bought_stocks_from_api()
 
 
+# the entire main and while True code loop was recommended by Artificial Intelligence
 def main():
-    #global stocks_to_buy
-    global bought_stocks
-    #stocks_to_buy = []
-
-    # the below code was recommended by Artificial Intelligence
     conn = initialize_database()
-
-    # I suggested the below line on my own to update the database with the api account list of owned stocks
-    bought_stocks = update_bought_stocks_from_api(conn)   # comment out this line if there are errors
-
-    # the below code was recommended by Artificial Intelligence
     stocks_to_buy = get_stocks_to_trade()
-    bought_stocks = load_bought_stocks_from_database(conn)
+    bought_stocks = update_bought_stocks_from_api(conn)
     buy_sell_lock = threading.Lock()
 
     # Create and start the buying and selling threads
@@ -272,35 +251,19 @@ def main():
     error_count = 0
     MAX_ERROR_COUNT = 5
 
-    #if not bought_stocks:
-      #  print("Database is empty. Please purchase at least 1 share of stock before running this Stock Market Robot.")
-     #   print("The Stock Market Robot requires that you own at least 1 share or a fractional share of stock ")
-      #  print("before you run the Stock Market Robot to allow the database to not be empty ")
-     #   print("because the database can not be created when it is completely empty. ")
-      #  print("A database needs to be created when this Stock Market Robot begins working ")
-     #   print("to keep track of all of the stock position buying and selling. ")
-     #   print("Thanks for understanding. Stocks can be purchased at the Alpaca website. ")
-     #   print("This software is not affiliated or endorsed by Alpaca Securities, LLC ")
-      #  print("This software does, however try to be a useful, profitable, and valuable ")
-      #  print("stock market trading application. ")
-       # bought_stocks = update_bought_stocks_from_api(conn)
-
     while True:
         try:
-            pass
-
             stop_if_stock_market_is_closed()
             now = datetime.now(pytz.timezone('US/Eastern'))
             current_time_str = now.strftime("Eastern Time, %m-%d-%Y,   %I:%M:%S %p")
             cash_balance = round(float(api.get_account().cash), 2)
-
             print(f"{current_time_str},    Cash Balance: ${cash_balance}")
+
             day_trade_count = api.get_account().daytrade_count
             print(f"Current day trade number: {day_trade_count} out of 3 in 5 business days")
 
             stocks_to_buy = get_stocks_to_trade()
             bought_stocks = load_bought_stocks_from_database(conn)
-
             if not bought_stocks:
                 bought_stocks = update_bought_stocks_from_api(conn)
 
