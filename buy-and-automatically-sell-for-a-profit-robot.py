99408228df2a5a4d93a60900a7b29f7ecacf09cf
codeprospecialist <droark28@hotmail.com>
2023-08-22 12:28:40 -0500

    Code bugs were fixed.

--- buy-and-automatically-sell-for-a-profit-robot.py
+++ buy-and-automatically-sell-for-a-profit-robot.py
@@ -7,7 +7,6 @@ from datetime import time as time2
 import alpaca_trade_api as tradeapi
 import pytz
 import talib
-import sqlite3
 import yfinance as yf
 
 # Load environment variables for Alpaca API
@@ -117,65 +116,8 @@ def get_average_true_range(symbol):
     return atr[-1]
 
 
-# the below database keeps track of bought and sold stocks, including dates
-# the below database was recommended by artificial intelligence
-def initialize_database():
-    conn = sqlite3.connect('stocks.db')
-    cursor = conn.cursor()
-    try:
-        cursor.execute("PRAGMA integrity_check;")
-        result = cursor.fetchone()
-        if result[0] != 'ok':
-            raise sqlite3.DatabaseError("Database is corrupted")
-    except sqlite3.DatabaseError:
-        os.remove('stocks.db')
-        conn = sqlite3.connect('stocks.db')
-        cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS bought_stocks
-                      (symbol TEXT PRIMARY KEY,
-                       price REAL,
-                       purchase_date TEXT)''')
-    conn.commit()
-    return conn
-
-
-# the below code was recommended by Artificial Intelligence
-def save_bought_stocks_to_database(bought_stocks, conn):
-    with conn: # Using context management for transactions
-        cursor = conn.cursor()
-        for symbol, (price, purchase_date) in bought_stocks.items():
-            cursor.execute("INSERT OR REPLACE INTO bought_stocks (symbol, price, purchase_date) VALUES (?, ?, ?)",
-                           (symbol, price, purchase_date))
-
-
-# the below code was recommended by Artificial Intelligence
-def load_bought_stocks_from_database(conn):
-    cursor = conn.cursor()
-    cursor.execute("SELECT symbol, price, purchase_date FROM bought_stocks")
-    bought_stocks = {row[0]: (row[1], datetime.strptime(row[2], "%Y-%m-%d").date()) for row in cursor.fetchall()}
-    return bought_stocks
-
-
-# the below code was recommended by Artificial Intelligence
-def update_bought_stocks_from_api(conn):
-    positions = api.list_positions()
-    bought_stocks = {}
-    for position in positions:
-        symbol = position.symbol
-        avg_entry_price = position.avg_entry_price
-        # Get the purchase date from the database if it exists
-        cursor = conn.cursor()
-        cursor.execute("SELECT purchase_date FROM bought_stocks WHERE symbol=?", (symbol,))
-        row = cursor.fetchone()
-        purchase_date = datetime.strptime(row[0], "%Y-%m-%d").date() if row else datetime.today().date()
-        bought_stocks[symbol] = (float(avg_entry_price), purchase_date)
-
-    save_bought_stocks_to_database(bought_stocks, conn)
-    return bought_stocks
-
-
 # the below function was recommended by Artificial Intelligence
-def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock, conn):
+def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock):
     stocks_to_remove = []
 
     for symbol in stocks_to_buy:
@@ -190,34 +132,41 @@ def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock, conn):
             api.submit_order(symbol=symbol, qty=qty_of_one_stock, side='buy', type='market', time_in_force='day')
             print(f" {today_date} , Bought {qty_of_one_stock} shares of {symbol} at {current_price}")
             logging.info(f" {today_date} , Bought {qty_of_one_stock} shares of {symbol} at {current_price}")
-
-            stocks_to_remove.append(symbol)
+            stocks_to_remove.append((symbol, current_price, today_date))  # Append tuple
 
     print(" Waiting 60 seconds after buying stock to update the database. ")
-    time.sleep(60)  # wait 60 seconds before updating the sqlite3 database
+    time.sleep(60)  # wait 60 seconds
 
     with buy_sell_lock:
-        for symbol, price, date in stocks_to_remove:
-            bought_stocks[symbol] = (round(current_price, 4), datetime.today().date())
+        for symbol, price, date in stocks_to_remove:  # Unpack tuple
+            bought_stocks[symbol] = (round(price, 4), date)
             stocks_to_buy.remove(symbol)
             remove_symbol_from_trade_list(symbol)
 
-            refresh_after_buy(conn)
-    # the below code was recommended by Artificial Intelligence
-    # line up the c in conn with the w in with
-    conn.close()     # Close connection outside the function with, for, or if loop to not have errors
+        refresh_after_buy()   # this is under the f in for
+
 
 
 # the below variable and list refresh function was recommended by Artificial Intelligence
-def refresh_after_buy(conn):
+def refresh_after_buy():
     global stocks_to_buy, bought_stocks
     stocks_to_buy = get_stocks_to_trade()
-    bought_stocks = update_bought_stocks_from_api(conn)   # need (conn)  here
+    bought_stocks = update_bought_stocks_from_api()
+
+
+def update_bought_stocks_from_api():
+    positions = api.list_positions()
+    bought_stocks = {}
+    for position in positions:
+        symbol = position.symbol
+        avg_entry_price = position.avg_entry_price
+        purchase_date = datetime.today().date()  # Set the date to today
+        bought_stocks[symbol] = (float(avg_entry_price), purchase_date)
+    return bought_stocks
 
 
 # the below function was recommended by Artificial Intelligence
-def sell_stocks(bought_stocks, buy_sell_lock, conn):
-
+def sell_stocks(bought_stocks, buy_sell_lock):
     stocks_to_remove = []
     for symbol, (bought_price, bought_date) in bought_stocks.items():
         current_price = get_current_price(symbol)
@@ -226,46 +175,30 @@ def sell_stocks(bought_stocks, buy_sell_lock, conn):
         if current_price >= atr_high_price and today_date > bought_date:
             qty = api.get_position(symbol).qty
             api.submit_order(symbol=symbol, qty=qty, side='sell', type='market', time_in_force='day')
-            del bought_stocks[symbol]
             print(f" {today_date}, Sold {qty} shares of {symbol} at {current_price} based on ATR high price")
             logging.info(f" {today_date}, Sold {qty} shares of {symbol} at {current_price} based on ATR high price")
-            stocks_to_remove.append(symbol)
+            stocks_to_remove.append(symbol)  # Append symbols to remove
 
     print(" Waiting 2 minutes after selling stock to allow the remote server to update the order in the account. ")
     time.sleep(120)  # sleep 120 seconds after each sell order
-    # the below code was recommended by Artificial Intelligence
+
     with buy_sell_lock:
         for symbol in stocks_to_remove:
-            del bought_stocks[symbol]
-        refresh_after_sell(conn)   # this lines up below the "f" in for
-
-    # the below code was recommended by Artificial Intelligence
-    # line up the c in conn with the w in with
-    conn.close()  # Close connection outside the function with, for, or if loop to not have errors
+            del bought_stocks[symbol]  # Delete symbols here
+        refresh_after_sell()   # this is under the f in for
 
 
 # the below variable and list refresh function was recommended by Artificial Intelligence
-def refresh_after_sell(conn):
+def refresh_after_sell():
     global bought_stocks
-    bought_stocks = update_bought_stocks_from_api(conn)
+    bought_stocks = update_bought_stocks_from_api()
 
 
 # the entire main and while True code loop was recommended by Artificial Intelligence
 def main():
-    conn = initialize_database()
     stocks_to_buy = get_stocks_to_trade()
-    bought_stocks = update_bought_stocks_from_api(conn)
-    buy_sell_lock = threading.Lock()
-
-    # Create and start the buying and selling threads
-    conn = sqlite3.connect('stocks.db')  # Create a database connection in the main thread
-    buy_thread = threading.Thread(target=buy_stocks, args=(bought_stocks, stocks_to_buy, buy_sell_lock, conn))
-    sell_thread = threading.Thread(target=sell_stocks, args=(bought_stocks, buy_sell_lock, conn))
-    buy_thread.start()
-    sell_thread.start()
-
-    error_count = 0
-    MAX_ERROR_COUNT = 5
+    bought_stocks = update_bought_stocks_from_api()
+    buy_sell_lock = threading.Lock()  # This lock may not be necessary in a single-threaded context
 
     while True:
         try:
@@ -279,9 +212,12 @@ def main():
             print(f"Current day trade number: {day_trade_count} out of 3 in 5 business days")
 
             stocks_to_buy = get_stocks_to_trade()
-            bought_stocks = load_bought_stocks_from_database(conn)
             if not bought_stocks:
-                bought_stocks = update_bought_stocks_from_api(conn)
+                bought_stocks = update_bought_stocks_from_api()
+
+            # Call the buy and sell functions
+            buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock)
+            sell_stocks(bought_stocks, buy_sell_lock)
 
             if DEBUG:
                 print("                                                                        ")
@@ -301,20 +237,14 @@ def main():
                     print(
                         f"Symbol: {symbol} | Current Price: {current_price} | ATR high sell signal profit price: {atr_high_price}")
                     print("----------------------------------------------------")
-            time.sleep(1)  # wait 1 second
+            time.sleep(1)  # this is under the i in if
 
 
-        except Exception as e:
+        except Exception as e:   # this is under the t in try
             logging.error(f"Error encountered: {e}")
-            error_count += 1
-            if error_count > MAX_ERROR_COUNT:
-                break
 
             time.sleep(2)
 
-        buy_thread.join()
-        sell_thread.join()
-        conn.close()  # Close the database connection
 
 if __name__ == '__main__':
     try:
