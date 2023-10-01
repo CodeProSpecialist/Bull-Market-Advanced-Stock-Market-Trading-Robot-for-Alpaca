293a27e5f1ae180f80f049141635a4cbdeb3e87d
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-10-01 09:14:20 -0500

    Update performance-stock-list-writer.py

--- performance-stock-list-writer.py
+++ performance-stock-list-writer.py
@@ -81,8 +81,8 @@ while True:
                 # Sort the stock data by percentage change in descending order
                 stock_data.sort(key=lambda x: x[1], reverse=True)
 
-                # Select the top 7 stocks
-                top_stocks = stock_data[:7]
+                # Select the top 3 stocks
+                top_stocks = stock_data[:3]
 
                 # Erase the existing file and write only the stock symbols to the output file
                 with open("electricity-or-utility-stocks-to-buy-list.txt", "w") as output_file:
