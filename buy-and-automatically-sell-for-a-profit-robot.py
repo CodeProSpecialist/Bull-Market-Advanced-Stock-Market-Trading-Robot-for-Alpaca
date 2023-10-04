7b942aa55658f57ed60b897e219293974afd29a9
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-10-03 19:54:00 -0500

    Update buy-and-automatically-sell-for-a-profit-robot.py

--- buy-and-automatically-sell-for-a-profit-robot.py
+++ buy-and-automatically-sell-for-a-profit-robot.py
@@ -413,8 +413,8 @@ def sell_stocks(bought_stocks, buy_sell_lock):
             bought_price = float(position.avg_entry_price)  # keep this under the "o" in "bought"
 
             # Never calculate ATR for a buy price or sell price because it is too slow. 1 second per stock.
-            # Sell stocks if the current price is more than 0.3% higher than the purchase price.
-            if current_price >= bought_price * 1.003:  # keep this under the "o" in "bought"
+            # Sell stocks if the current price is more than 2.5% higher than the purchase price.
+            if current_price >= bought_price * 1.025:  # keep this under the "o" in "bought"
                 qty = api.get_position(symbol).qty
                 api.submit_order(symbol=symbol, qty=qty, side='sell', type='market', time_in_force='day')
                 print(f" {current_time_str}, Sold {qty} shares of {symbol} at {current_price} based on a higher selling price. ")
