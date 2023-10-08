b83d68eab66cc662a5b275e7f84b4d922713afa8
codeprospecialist <droark28@hotmail.com>
2023-10-08 14:50:34 -0500

    Code bugs were fixed.

--- buy-and-automatically-sell-for-a-profit-robot.py
+++ buy-and-automatically-sell-for-a-profit-robot.py
@@ -309,7 +309,7 @@ def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock):
             print(f" {current_time_str} , Bought {qty_of_one_stock} shares of {symbol} at {current_price}")
             with open(csv_filename, mode='a', newline='') as csv_file:
                 csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                csv_writer.writerow({'Timestamp': current_time_str, 'Buy or Sell': 'Buy', 'Quantity': qty_of_one_stock, 'Symbol': symbol, 'Price Per Share': current_price})
+                csv_writer.writerow({'Date': current_time_str, 'Buy or Sell': 'Buy', 'Quantity': qty_of_one_stock, 'Symbol': symbol, 'Price Per Share': current_price})
             
             stocks_to_remove.append((symbol, current_price, today_date_str))  # Append tuple
             # the database requires the datetime date format from today_date
@@ -437,7 +437,7 @@ def sell_stocks(bought_stocks, buy_sell_lock):
                 print(f" {current_time_str}, Sold {qty} shares of {symbol} at {current_price} based on a higher selling price. ")
                 with open(csv_filename, mode='a', newline='') as csv_file:
                     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                    csv_writer.writerow({'Timestamp': current_time_str, 'Buy or Sell': 'Sell', 'Quantity': qty, 'Symbol': symbol, 'Price Per Share': current_price})
+                    csv_writer.writerow({'Date': current_time_str, 'Buy or Sell': 'Sell', 'Quantity': qty, 'Symbol': symbol, 'Price Per Share': current_price})
                 
                 stocks_to_remove.append(symbol)  # Append symbols to remove
 
