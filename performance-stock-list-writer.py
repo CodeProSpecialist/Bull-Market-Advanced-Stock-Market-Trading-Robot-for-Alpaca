52144fc0a3d859fb8afedaffdba4a7e80f166319
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-10-12 18:41:13 -0500

    Update performance-stock-list-writer.py

--- performance-stock-list-writer.py
+++ performance-stock-list-writer.py
@@ -3,22 +3,31 @@ import pytz
 import yfinance as yf
 from datetime import datetime, timedelta
 
-# Modify the calculate_monthly_percentage_changes function to consider the past 2 months this year
+# Function to calculate percentage change for a given timeframe
+def calculate_percentage_change(stock, period):
+    history = stock.history(period=period)
+    
+    if history.empty:
+        return 0  # Default value when data is unavailable
+    
+    start_price = history['Open'][0]
+    end_price = history['Close'][-1]
+    percentage_change = ((end_price - start_price) / start_price) * 100
+    return percentage_change
+
+# Function to calculate monthly percentage changes
 def calculate_monthly_percentage_changes(stock, current_month, current_year):
-    # Retrieve historical data for the past 1 year
     history = stock.history(period="1y")
     
-    # Create a dictionary to store percentage changes for the past 2 months this year
     monthly_percentage_changes = {}
     
-    for month in range(current_month - 2, current_month + 1):  # Consider the past 2 months and the current month
+    for month in range(current_month - 2, current_month + 1):
         if month <= 0:
-            month += 12  # Handle year rollover
+            month += 12
             year = current_year - 1
         else:
             year = current_year
         
-        # Filter data for the current month
         monthly_data = history[(history.index.month == month) & (history.index.year == year)]
         
         if not monthly_data.empty:
@@ -42,7 +51,7 @@ while True:
         eastern = pytz.timezone('US/Eastern')
         now = datetime.now(eastern)
         
-        if run_count == 1 or (now.weekday() in [0, 1, 2, 3, 4] and now.time() >= start_time and now.time() <= end_time):
+        if run_count == 1 or (now.weekday() in [0, 1, 2, 3, 4] and start_time <= now.time() <= end_time):
             # Increment run count
             run_count += 1
 
@@ -50,92 +59,45 @@ while True:
             with open("list-of-stock-symbols-to-scan.txt", "r") as input_file:
                 stock_symbols = [line.strip() for line in input_file if line.strip()]
             
-            # Initialize a dictionary to store stock data
-            stock_data = {}
-            
-            # Fetch historical data and calculate percentage changes for each stock
+            # Initialize a list to store filtered stocks
+            filtered_stocks = []
+
             for symbol in stock_symbols:
                 stock = yf.Ticker(symbol)
                 print(f"Downloading the historical data for {symbol}...")
                 
-                # Fetch historical data for the past 1 day
-                history_1_day = stock.history(period="1d")
-                
-                # Calculate percentage change for the past 1 day (or use default value if data is unavailable)
-                if not history_1_day.empty:
-                    start_price_1_day = history_1_day['Open'][0]
-                    end_price_1_day = history_1_day['Close'][-1]
-                    percentage_change_1_day = ((end_price_1_day - start_price_1_day) / start_price_1_day) * 100
-                else:
-                    percentage_change_1_day = 0  # Default value when data is unavailable
-
-                # You can repeat similar checks and calculations for other timeframes (1 week, 14 days, 1 month)
-
-                # Fetch historical data for the past 1 week
-                history_1_week = stock.history(period="1wk")
-                if not history_1_week.empty:
-                    start_price_1_week = history_1_week['Open'][0]
-                    end_price_1_week = history_1_week['Close'][-1]
-                    percentage_change_1_week = ((end_price_1_week - start_price_1_week) / start_price_1_week) * 100
-                else:
-                    percentage_change_1_week = 0
-
-                # Fetch historical data for the past 14 days
-                history_14_days = stock.history(period="14d")
-                if not history_14_days.empty:
-                    start_price_14_days = history_14_days['Open'][0]
-                    end_price_14_days = history_14_days['Close'][-1]
-                    percentage_change_14_days = ((end_price_14_days - start_price_14_days) / start_price_14_days) * 100
-                else:
-                    percentage_change_14_days = 0
-
-                # Fetch historical data for the past 1 month
-                history_1_month = stock.history(period="1mo")
-                if not history_1_month.empty:
-                    start_price_1_month = history_1_month['Open'][0]
-                    end_price_1_month = history_1_month['Close'][-1]
-                    percentage_change_1_month = ((end_price_1_month - start_price_1_month) / start_price_1_month) * 100
-                else:
-                    percentage_change_1_month = 0
+                # Calculate percentage changes for different timeframes
+                percentage_change_1_day = calculate_percentage_change(stock, "1d")
+                percentage_change_1_week = calculate_percentage_change(stock, "1wk")
+                percentage_change_14_days = calculate_percentage_change(stock, "14d")
+                percentage_change_1_month = calculate_percentage_change(stock, "1mo")
 
                 # Retrieve monthly percentage changes
                 monthly_percentage_changes = calculate_monthly_percentage_changes(stock, now.month, now.year)
-                
-                stock_data[symbol] = {
-                    'percentage_change_1_day': percentage_change_1_day,
-                    'percentage_change_1_week': percentage_change_1_week,
-                    'percentage_change_14_days': percentage_change_14_days,
-                    'percentage_change_1_month': percentage_change_1_month,
-                    'monthly_percentage_changes': monthly_percentage_changes
-                }
-                time.sleep(2)  # Sleep for 2 seconds
-                
-            # Filter stocks based on historical performance for the current month and the past 2 months
-            current_month = now.month
-            current_year = now.year
 
-            filtered_stocks = []
-            for symbol, data in stock_data.items():
+                # Check if the stock meets the filtering criteria
                 if (
-                    current_month in data['monthly_percentage_changes'] and
-                    current_month - 1 in data['monthly_percentage_changes'] and
-                    current_month - 2 in data['monthly_percentage_changes'] and
-                    data['monthly_percentage_changes'][current_month] > 0 and
-                    data['monthly_percentage_changes'][current_month - 1] > 0 and
-                    data['monthly_percentage_changes'][current_month - 2] > 0 and
-                    data['percentage_change_1_day'] >= 1 and
-                    data['percentage_change_1_week'] >= 1 and
-                    data['percentage_change_14_days'] >= 1 and
-                    data['percentage_change_1_month'] >= 1
+                    current_month in monthly_percentage_changes
+                    and current_month - 1 in monthly_percentage_changes
+                    and current_month - 2 in monthly_percentage_changes
+                    and monthly_percentage_changes[current_month] > 0
+                    and monthly_percentage_changes[current_month - 1] > 0
+                    and monthly_percentage_changes[current_month - 2] > 0
+                    and percentage_change_1_day >= 1
+                    and percentage_change_1_week >= 1
+                    and percentage_change_14_days >= 1
+                    and percentage_change_1_month >= 1
                 ):
-                    filtered_stocks.append((symbol, data['percentage_change_1_day']))
-            
+                    filtered_stocks.append((symbol, percentage_change_1_day))
+                
+                time.sleep(2)  # Sleep for 2 seconds
+
             # Sort the filtered stocks by percentage change for the current day
             sorted_stocks = sorted(filtered_stocks, key=lambda x: x[1], reverse=True)
-            
+
             # Select the top 28 stocks
             top_stocks = sorted_stocks[:28]
-            
+
             # Write the selected stock symbols to the output file
             with open("electricity-or-utility-stocks-to-buy-list.txt", "w") as output_file:
                 for symbol, _ in top_stocks:
@@ -152,7 +114,7 @@ while True:
         else:
             next_run = now + timedelta(minutes=5)
             next_run = next_run.replace(second=0, microsecond=0)
-        
+
         main_message = f"Next run will be soon after the time of {next_run.strftime('%I:%M %p')} (Eastern Time)."
         print(main_message)
         print("")
