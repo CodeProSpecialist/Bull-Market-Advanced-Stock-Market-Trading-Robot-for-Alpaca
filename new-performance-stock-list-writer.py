a996c6759bfaf6fe8576951d8b3083b090905d0f
codeprospecialist <droark28@hotmail.com>
2023-11-04 17:15:49 -0500

    Code bugs were fixed.

--- new-performance-stock-list-writer.py
+++ new-performance-stock-list-writer.py
@@ -20,15 +20,27 @@ def get_top_increase_stocks(symbols):
             top_stocks[symbol] = price_increase
     return dict(sorted(top_stocks.items(), key=lambda item: item[1], reverse=True))
 
+
+# Function to print the top increase stocks to the terminal with current price
+def print_top_stocks(top_stocks):
+    rank = 1
+    for symbol, price_increase in top_stocks.items():
+        stock = yf.Ticker(symbol)
+        current_price = stock.history(period='1d')['Close'].iloc[0]
+        percent_change = price_increase * 100
+        change_symbol = '+' if percent_change > 0 else '-'
+        print(f"{rank}. {symbol}: ${current_price:.2f}, {change_symbol}{abs(percent_change):.2f}%")
+        rank += 1
+
+
 # Function to write the top increase stocks to an output file
 def write_top_stocks_to_file(filename, top_stocks):
     with open(filename, 'w') as file:
-        rank = 1
         for symbol, price_increase in top_stocks.items():
             percent_change = price_increase * 100
-            change_symbol = '+' if percent_change > 0 else '-'
-            file.write(f"{rank}. {symbol}: {change_symbol}{abs(percent_change):.2f}%\n")
-            rank += 1
+            if percent_change > 0:
+                file.write(f"{symbol}\n")
+
 
 if __name__ == "__main__":
     input_filename = "list-of-stock-symbols-to-scan.txt"
@@ -40,17 +52,24 @@ if __name__ == "__main__":
     while True:
         # Get the current date and time in Eastern Time
         current_time = datetime.now(et)
-        formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+        formatted_time = current_time.strftime('%I:%M:%S %p | %m-%d-%Y')
 
-        print(f"Eastern Time: {formatted_time}")
-
+        print("")
+        print(f" Eastern Time: {formatted_time} ")
+        print("")
         stocks_to_scan = read_stock_symbols(input_filename)
         top_increase_stocks = get_top_increase_stocks(stocks_to_scan)
 
+        # Print the top increase stocks to the terminal
+        print_top_stocks(top_increase_stocks)
+
+        print("")
+        print("Writing the list of stocks: ")
         # Write the top increase stocks to the output file and display on the screen
         write_top_stocks_to_file(output_filename, top_increase_stocks)
         for line in open(output_filename, 'r'):
             print(line, end='')
 
+        print("")
         # Sleep for 30 seconds before the next update
         time.sleep(30)
