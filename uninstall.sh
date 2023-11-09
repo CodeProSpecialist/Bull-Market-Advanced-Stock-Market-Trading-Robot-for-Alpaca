af61666d904331a3d97abbd910c04bbd41b62ee6
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-11-09 09:30:35 -0600

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-echo "Uninstalling the extra software libraries that were installed."
-
-sudo pip3 uninstall yfinance alpaca-trade-api sqlalchemy pytz ta-lib 
-
-echo "Uninstall completed successfully."
