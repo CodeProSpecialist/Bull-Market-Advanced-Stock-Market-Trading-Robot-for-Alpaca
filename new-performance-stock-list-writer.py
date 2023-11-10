f476a86360c8f4cebd117486e0b279c2c24c28de
codeprospecialist <droark28@hotmail.com>
2023-11-10 13:55:18 -0600

    Code bugs were fixed.

--- new-performance-stock-list-writer.py
+++ new-performance-stock-list-writer.py
@@ -13,11 +13,15 @@ def read_stock_symbols(filename):
 def get_top_increase_stocks(symbols):
     top_stocks = {}
     for symbol in symbols:
-        stock = yf.Ticker(symbol)
-        hist_data = stock.history(period='14d')
-        if len(hist_data) == 14:
-            price_increase = (hist_data['Close'][-1] - hist_data['Close'][0]) / hist_data['Close'][0]
-            top_stocks[symbol] = price_increase
+        try:
+            stock = yf.Ticker(symbol)
+            hist_data = stock.history(period='14d')
+            if len(hist_data) == 14:
+                price_increase = (hist_data['Close'][-1] - hist_data['Close'][0]) / hist_data['Close'][0]
+                top_stocks[symbol] = price_increase
+        except Exception as e:
+            print(f"Error retrieving data for {symbol}: {e}")
+
     time.sleep(1)
     return dict(sorted(top_stocks.items(), key=lambda item: item[1], reverse=True))
 
@@ -26,13 +30,16 @@ def get_top_increase_stocks(symbols):
 def print_top_stocks(top_stocks):
     rank = 1
     for symbol, price_increase in top_stocks.items():
-        stock = yf.Ticker(symbol)
-        current_price = stock.history(period='1d')['Close'].iloc[0]
-        percent_change = price_increase * 100
-        change_symbol = '+' if percent_change > 0 else '-'
-        print(f"{rank}. {symbol}: ${current_price:.2f}, {change_symbol}{abs(percent_change):.2f}%")
-        rank += 1
-        time.sleep(1)
+        try:
+            stock = yf.Ticker(symbol)
+            current_price = stock.history(period='1d')['Close'].iloc[0]
+            percent_change = price_increase * 100
+            change_symbol = '+' if percent_change > 0 else '-'
+            print(f"{rank}. {symbol}: ${current_price:.2f}, {change_symbol}{abs(percent_change):.2f}%")
+            rank += 1
+            time.sleep(1)
+        except Exception as e:
+            print(f"Error printing data for {symbol}: {e}")
 
 # Function to write the top increase stocks to an output file
 def write_top_stocks_to_file(filename, top_stocks):
@@ -42,7 +49,6 @@ def write_top_stocks_to_file(filename, top_stocks):
             if percent_change > 5:
                 file.write(f"{symbol}\n")
 
-
 if __name__ == "__main__":
     input_filename = "list-of-stock-symbols-to-scan.txt"
     output_filename = "electricity-or-utility-stocks-to-buy-list.txt"
@@ -51,28 +57,33 @@ if __name__ == "__main__":
     et = pytz.timezone('US/Eastern')
 
     while True:
-        # Get the current date and time in Eastern Time
-        current_time = datetime.now(et)
-        formatted_time = current_time.strftime('%I:%M:%S %p | %m-%d-%Y')
+        try:
+            # Get the current date and time in Eastern Time
+            current_time = datetime.now(et)
+            formatted_time = current_time.strftime('%I:%M:%S %p | %m-%d-%Y')
 
-        print("")
-        print(f" Eastern Time: {formatted_time} ")
-        print("")
+            print("")
+            print(f" Eastern Time: {formatted_time} ")
+            print("")
 
-        stocks_to_scan = read_stock_symbols(input_filename)
-        top_increase_stocks = get_top_increase_stocks(stocks_to_scan)
+            stocks_to_scan = read_stock_symbols(input_filename)
+            top_increase_stocks = get_top_increase_stocks(stocks_to_scan)
 
-        # Print the top increase stocks to the terminal
-        print_top_stocks(top_increase_stocks)
+            # Print the top increase stocks to the terminal
+            print_top_stocks(top_increase_stocks)
 
-        print("")
-        print("Writing the list of stocks: ")
-        print("")
-        # Write the top increase stocks to the output file and display on the screen
-        write_top_stocks_to_file(output_filename, top_increase_stocks)
-        for line in open(output_filename, 'r'):
-            print(line, end='')
+            print("")
+            print("Writing the list of stocks: ")
+            print("")
+            # Write the top increase stocks to the output file and display on the screen
+            write_top_stocks_to_file(output_filename, top_increase_stocks)
+            for line in open(output_filename, 'r'):
+                print(line, end='')
 
-        print("")
-        # Sleep for 30 seconds before the next update
-        time.sleep(30)
+            print("")
+            # Sleep for 30 seconds before the next update
+            time.sleep(30)
+        except Exception as e:
+            print(f"Error in the main loop: {e}")
+            print("Restarting the script in 1 minute...")
+            time.sleep(60)
