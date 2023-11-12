51771fec80eb2397927480821c36c0289c3a856e
codeprospecialist <droark28@hotmail.com>
2023-11-12 05:29:24 -0600

    Code bugs were fixed.

--- stock-market-robot.py
+++ stock-market-robot.py
@@ -336,6 +336,11 @@ def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock):
     stocks_to_remove = []
     global start_time, end_time, original_start_time  # Access the global end_time variable
 
+    now = datetime.now(pytz.timezone('US/Eastern'))
+
+    # debugging code below to run the robot at different times
+    #start_trading_time = datetime.now().replace(hour=6, minute=26, second=0, microsecond=0)
+
     # Define the target time as 12:30 Eastern Time
     start_trading_time = datetime.now().replace(hour=12, minute=30, second=0, microsecond=0)
 
