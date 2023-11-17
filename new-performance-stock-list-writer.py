2c8369c53b48320503f7a11e1aa6e799fe833b2b
codeprospecialist <droark28@hotmail.com>
2023-11-17 11:41:24 -0600

    Code bugs were fixed.

--- new-performance-stock-list-writer.py
+++ new-performance-stock-list-writer.py
@@ -3,26 +3,31 @@ import yfinance as yf
 from datetime import datetime
 import pytz  # You'll need to install pytz if you haven't already
 
+
 # Function to read the list of stock symbols from a file
 def read_stock_symbols(filename):
     with open(filename, 'r') as file:
         symbols = [line.strip() for line in file]
     return symbols
 
-# Function to get the top price increase stocks for the past 2 days
+
+# Function to get the top price increase stocks for the past 1 day
 def get_top_increase_stocks(symbols):
     top_stocks = {}
     for symbol in symbols:
         try:
             stock = yf.Ticker(symbol)
-            hist_data = stock.history(period='2d')
-            if len(hist_data) == 2:
-                price_increase = (hist_data['Close'][-1] - hist_data['Close'][0]) / hist_data['Close'][0]
+
+            # Fetch data for only today
+            hist_data = stock.history(period='1d')
+
+            if not hist_data.empty:
+                price_increase = (hist_data['Close'].iloc[-1] - hist_data['Close'].iloc[0]) / hist_data['Close'].iloc[0]
                 top_stocks[symbol] = price_increase
         except Exception as e:
             print(f"Error retrieving data for {symbol}: {e}")
 
-    time.sleep(1)
+        time.sleep(1)
     return dict(sorted(top_stocks.items(), key=lambda item: item[1], reverse=True))
 
 
@@ -32,6 +37,7 @@ def print_top_stocks(top_stocks):
     for symbol, price_increase in top_stocks.items():
         try:
             stock = yf.Ticker(symbol)
+            # Fetch current price for today
             current_price = stock.history(period='1d')['Close'].iloc[0]
             percent_change = price_increase * 100
             change_symbol = '+' if percent_change > 0 else '-'
@@ -41,6 +47,7 @@ def print_top_stocks(top_stocks):
         except Exception as e:
             print(f"Error printing data for {symbol}: {e}")
 
+
 # Function to write the top increase stocks to an output file
 def write_top_stocks_to_file(filename, top_stocks):
     with open(filename, 'w') as file:
@@ -48,6 +55,7 @@ def write_top_stocks_to_file(filename, top_stocks):
             percent_change = price_increase * 100
             if percent_change > 0.7:
                 file.write(f"{symbol}\n")
+
 
 if __name__ == "__main__":
     input_filename = "list-of-stock-symbols-to-scan.txt"
@@ -59,11 +67,8 @@ if __name__ == "__main__":
     while True:
         try:
             # Get the current date and time in Eastern Time
-            current_time = datetime.now(et)
-            formatted_time = current_time.strftime('%I:%M:%S %p | %m-%d-%Y')
-
             print("")
-            print(f" Eastern Time: {formatted_time} ")
+            print(f" Eastern Time: {datetime.now(et).strftime('%I:%M:%S %p | %m-%d-%Y')} ")
             print("")
 
             stocks_to_scan = read_stock_symbols(input_filename)
