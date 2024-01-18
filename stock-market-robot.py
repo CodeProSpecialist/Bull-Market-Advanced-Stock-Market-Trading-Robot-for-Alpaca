b17e8710ec9b758e5352c94a1616ddb9b417b255
codeprospecialist <droark28@hotmail.com>
2024-01-18 09:56:50 -0600

    Code bugs were fixed.
    Signed-off-by: codeprospecialist <droark28@hotmail.com>

--- stock-market-robot.py
+++ stock-market-robot.py
@@ -385,8 +385,10 @@ def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock):
     # debugging code below to run the robot at different times
     # start_trading_time = datetime.now(pytz.timezone('US/Eastern')).replace(hour=5, minute=30, second=0, microsecond=0)
 
-    # Define the target time as 9:30am Eastern Time to begin looking for stocks to buy
-    start_trading_time = datetime.now(pytz.timezone('US/Eastern')).replace(hour=9, minute=28, second=0, microsecond=0)
+    # Define the target time as 10:30am Eastern Time to begin looking for stocks to buy
+    # 10:30 am is a wise start trading target time because the trading volume has built up for 1 hour.
+    # Professional stock market traders on Wall St. often wait 1 hour before buying stocks.
+    start_trading_time = datetime.now(pytz.timezone('US/Eastern')).replace(hour=10, minute=30, second=0, microsecond=0)
 
     # print("datetime.now as compared to start_trading_time: ",datetime.now(pytz.timezone('US/Eastern')))     # uncomment this line to debug the code
     # print("start_trading_time to start buying stocks: ",start_trading_time)      # uncomment this line to debug the code
