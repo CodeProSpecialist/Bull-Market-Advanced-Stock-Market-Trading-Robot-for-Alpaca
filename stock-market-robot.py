0e2710cbc76fde92a12dbe623370dcd74d9ef39e
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-04-24 18:22:56 -0500

    Update stock-market-robot.py

--- stock-market-robot.py
+++ stock-market-robot.py
@@ -558,6 +558,8 @@ def buy_stocks(bought_stocks, stocks_to_buy, buy_sell_lock):
                         print("")
                         print_technical_indicators(symbol, calculate_technical_indicators(symbol))
                         print("")
+                        # Convert symbol from BRK-B to BRK.B if necessary
+                        symbol = symbol.replace('-', '.')
                         api.submit_order(symbol=symbol, qty=qty_of_one_stock, side='buy', type='market',
                                          time_in_force='day')
 
