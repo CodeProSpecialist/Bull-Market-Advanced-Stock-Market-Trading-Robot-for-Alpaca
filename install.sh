7f86af5c643a889ba8d4acb03173f42bf6070fe5
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-04-28 09:51:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,20 +62,33 @@ conda init bash
 conda activate
 
 # Install Python packages within the virtual environment
-pip3 install yfinance alpaca-trade-api sqlalchemy pytz ta-lib schedule
+pip3 install yfinance 
+
+pip3 install alpaca-trade-api 
+
+pip3 install sqlalchemy 
+
+pip3 install pytz 
+
+pip3 install ta-lib 
+
+pip3 install schedule
+
+echo "All done! You can now run your Python script with Anaconda."
 
 # Inform the user about Anaconda installation
 echo "Your Python commands will be the Python commands that run with Anaconda's Python programs."
 echo "You can activate Anaconda by running 'conda activate' and then install anything else with pip3 ."
 
 # Inform the user about the virtual environment
-echo "Your Python commands in the directory for Anaconda will be the Python commands that run this installed virtual environment's Python programs."
-
-#echo "All done! You can now run your Python script with Anaconda."
+#echo "Your Python commands in the directory for Anaconda will be the Python commands that run this installed virtual environment's Python programs."
 
 echo "type:   conda activate  " 
 
 echo "type:    pip3 install yfinance alpaca-trade-api sqlalchemy pytz ta-lib schedule"
 
-echo "Then the python 3 packages installation is complete. "
+echo "View the installed pip3 packages with the command: pip3 list  "
 
+echo "I have found that pip3 will prefer to install 1 package at a time. Try this. "
+
+echo "Then the python 3 packages installation is complete. "
